996f6b4c04abbedc934426471eb978cc08364006
Haonan Hu <hhn97@ku.edu>
2020-02-15 23:00:51 -0600

    Done quadratic probing

--- HashTable.cpp
+++ HashTable.cpp
@@ -10,14 +10,29 @@ Brief:File is header file
 HashTable::HashTable()
 {
   m_size = 11;
-  m_arrObjs = 0;
-  m_arr = new User[11];
+  m_arrLinear = new User[11];
+  m_arrQuadratic = new User[11];
 }
 
 HashTable::~HashTable()
 {
-  delete[] m_arr;
+  delete[] m_arrLinear;
 }
+
+int HashTable::getLambda()
+{
+  int factor = 0;
+  for(int i = 0; i < m_size; i++)
+  {
+    if(m_arrLinear[i].getName() != "")
+    {
+      factor++;
+    }
+  }
+  lambda = factor / m_size;
+  return lambda;
+}
+
 int HashTable::linerProbing(std::string password)
 {
   int index1 = 0;
@@ -39,7 +54,7 @@ int HashTable::linerProbing(std::string password)
   hashNum++;
   index1 =sum % 11;
 //  std::cout << index1 << '\n';
-  if(m_arr[index1].getName() == "")
+  if(m_arrLinear[index1].getName() == "")
   {
     return index1;
   }
@@ -47,7 +62,7 @@ int HashTable::linerProbing(std::string password)
   {
     // case 2: Have collision
     index2 = index1;
-    while(m_arr[index2].getName() != "")
+    while(m_arrLinear[index2].getName() != "")
     {
       index2 = (index1 + hashNum) % 11;
       hashNum++;
@@ -57,28 +72,86 @@ int HashTable::linerProbing(std::string password)
   }
 }
 
+int HashTable::quadraticProbing(std::string password)
+{
+  int index1 = 0;
+  int index2 = 0;
+  int sum = 0;
+  int hashNum = 0;
+  int passwordLength = password.length();
+  //base case 1: No collision after first try
+  for (int i = 0; i < passwordLength; i++)
+  {
+      int value = (int)password.at(i);
+      if( value >= 48 && value <= 57)
+      {
+        value = value - (int)'0'; // convert char to int
+      }
+      sum = sum + value;
+//     std::cout << sum << '\n';
+  }
+  hashNum++;
+  index1 =sum % 11;
+//  std::cout << index1 << '\n';
+  if(m_arrLinear[index1].getName() == "")
+  {
+    return index1;
+  }
+  else
+  {
+    // case 2: Have collision
+    index2 = index1;
+    while(m_arrLinear[index2].getName() != "")
+    {
+      index2 = (index1 + hashNum * hashNum) % 11;
+      hashNum++;
+    }
+  //  std::cout << index2 << '\n';
+    return index2;
+  }
+}
+
 void HashTable::addUser(std::string userName, std::string password)
 {
-  int index = linerProbing(password);
+  int indexLinear = linerProbing(password);
+  int indexQuadratic = quadraticProbing(password);
 //  std::cout << index << '\n';
-  m_arr[index].setName(userName);
-  m_arr[index].setPassword(password);
-//  std::cout << m_arr[index].getName() << ',' << m_arr[index].getPassword();
+  m_arrLinear[indexLinear].setName(userName);
+  m_arrLinear[indexLinear].setPassword(password);
+  m_arrQuadratic[indexQuadratic].setName(userName);
+  m_arrQuadratic[indexQuadratic].setPassword(password);
+//  std::cout << m_arrLinear[index].getName() << ',' << m_arrLinear[index].getPassword();
 }
 
 void HashTable::printUser()
 {
+  std::cout << "\e[4mLinear Probing:\e[0m\n";
   for(int i = 0; i < m_size; i++)
   {
     std::cout << i << ':';
-    if(m_arr[i].getName() == "")
+    if(m_arrLinear[i].getName() == "")
     {
       std::cout << "\n";
     }
     else
     {
-      std::cout << m_arr[i].getName() << ':';
-      std::cout << m_arr[i].getPassword() << '\n';
+      std::cout << m_arrLinear[i].getName() << ':';
+      std::cout << m_arrLinear[i].getPassword() << '\n';
+    }
+  }
+  std::cout << "\n\n\n";
+  std::cout << "\e[4mQuadratic Probing:\e[0m\n";
+  for(int i = 0; i < m_size; i++)
+  {
+    std::cout << i << ':';
+    if(m_arrQuadratic[i].getName() == "")
+    {
+      std::cout << "\n";
+    }
+    else
+    {
+      std::cout << m_arrQuadratic[i].getName() << ':';
+      std::cout << m_arrQuadratic[i].getPassword() << '\n';
     }
   }
 }
--- HashTable.h
+++ HashTable.h
@@ -19,11 +19,12 @@ public:
   void addUser(std::string userName, std::string password);
   void removeUser(std::string userName, std::string password);
   void printUser();
+  int getLambda();
 private:
-  User* m_arr; // User objects array
+  User* m_arrLinear; // linear probing method hash table
+  User* m_arrQuadratic; // quadratic probing method hash table 
   int m_size; // HashTable size
-  int m_arrObjs; // number of items in the array
-  double load_factor = 0.0; // m_arrObjs / m_size
+  double lambda = 0.0; // m_arrObjs / m_size
   int linerProbing(std::string password);
   int quadraticProbing(std::string password);
 };
